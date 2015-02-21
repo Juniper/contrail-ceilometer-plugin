9171eb0b0ac956465e1887edb213b936188b25d9
Megh Bhatt <meghb@juniper.net>
2015-02-20 17:01:38 -0800

    OpenContrail ceilometer plugin commit for icehouse: 1. Add floating IP meters - ip.floating.receive.bytes,    ip.floating.receive.packets, ip.floating.transmit.bytes,    ip.floating.transmit.packets. 2. Backport the OpenContrail ceilometer driver from juno into    icehouse. Also brought in neutron_client.py. 3. Add support for floating IP meters in the OpenContrail driver.

---
+++ setup.py
@@ -0,0 +1,40 @@
+#
+# Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+#
+
+import setuptools, re
+
+def requirements(filename):
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    c = re.compile(r'\s*#.*')
+    return filter(bool, map(lambda y: c.sub('', y).strip(), lines))
+
+setuptools.setup(
+    name='ceilometer-plugin-contrail',
+    version='0.1dev',
+    packages=setuptools.find_packages(),
+    package_data={'': ['*.html', '*.css', '*.xml']},
+
+    # metadata
+    author="OpenContrail",
+    author_email="dev@lists.opencontrail.org",
+    license="Apache Software License",
+    url="http://www.opencontrail.org/",
+
+    long_description="OpenContrail Ceilometer Plugin",
+
+    install_requires=requirements('requirements.txt'),
+
+    entry_points = {
+        'ceilometer.poll.central' : [
+            'ip.floating.receive.packets = ceilometer_plugin_contrail.network.statistics.floatingip:FloatingIPPollsterReceivePackets',
+            'ip.floating.transmit.packets = ceilometer_plugin_contrail.network.statistics.floatingip:FloatingIPPollsterTransmitPackets',
+            'ip.floating.receive.bytes = ceilometer_plugin_contrail.network.statistics.floatingip:FloatingIPPollsterReceiveBytes',
+            'ip.floating.transmit.bytes = ceilometer_plugin_contrail.network.statistics.floatingip:FloatingIPPollsterTransmitBytes',
+        ],
+        'network.statistics.drivers' : [
+            'opencontrail = ceilometer_plugin_contrail.network.statistics.opencontrail.driver:OpenContrailDriver'
+        ],
+    },
+)
