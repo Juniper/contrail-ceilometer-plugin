1978d4e6e7e82408ae21d6852843997044edb952
Megh Bhatt <meghb@juniper.net>
2016-05-09 16:45:34 -0700

    Move to oslo_XXX
    Closes-Bug: #1578817

--- ceilometer_plugin_contrail/network/statistics/contrail/driver.py
+++ ceilometer_plugin_contrail/network/statistics/contrail/driver.py
@@ -13,7 +13,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from oslo.utils import timeutils
+from oslo_utils import timeutils
 from six.moves.urllib import parse as urlparse
 
 from ceilometer.network.statistics import driver
